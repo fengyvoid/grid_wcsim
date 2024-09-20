8387c6734e19dc155b61bda24cf7f22ee03577f9
Steven Doran <78985334+S81D@users.noreply.github.com>
2024-09-20 10:14:57 -0500

    Update submit_wcsim_job.sh
    another one bites the dust

--- submit_wcsim_job.sh
+++ submit_wcsim_job.sh
@@ -13,5 +13,5 @@ OUTPUT_FOLDER=/pnfs/annie/scratch/users/doran/output/genie_muons/${BATCH}
 mkdir -p $OUTPUT_FOLDER                                                 
 
 # wrapper script to submit your grid job
-jobsub_submit --memory=2000MB --expected-lifetime=6h -G annie --disk=5GB --resource-provides=usage_model=OFFSITE --blacklist=Omaha,Swan,Wisconsin,SU-ITS -f ${INPUT_PATH}/WCSim.tar.gz -f ${INPUT_PATH}/wcsim_container.sh -f ${INPUT_PATH}/submit/${RUN}/WCSim.mac -d OUTPUT $OUTPUT_FOLDER file://${INPUT_PATH}/run_job.sh ${RUN} ${BATCH}
+jobsub_submit --memory=2000MB --expected-lifetime=6h -G annie --disk=5GB --resource-provides=usage_model=OFFSITE --blacklist=Omaha,Swan,Wisconsin,SU-ITS,RAL -f ${INPUT_PATH}/WCSim.tar.gz -f ${INPUT_PATH}/wcsim_container.sh -f ${INPUT_PATH}/submit/${RUN}/WCSim.mac -d OUTPUT $OUTPUT_FOLDER file://${INPUT_PATH}/run_job.sh ${RUN} ${BATCH}
 
